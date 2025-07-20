92e8a30977fa5c2e224997d3eec6bc427b3ab479
Professeur-xd <xdprofesseur@gmail.com>
2025-07-20 01:33:11 -0400

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "50941319791",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "528145550802",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "𝐏𝐑𝐈𝐍𝐂𝐄 𝐗𝐓𝐑𝐄𝐌𝐄",
 // add bot owner name
